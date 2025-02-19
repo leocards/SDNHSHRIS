dfe5395b3f6ef78d9ae2125887637182ab38df6b
leocards <leomascardenio10@gmail.com>
2025-02-19 17:12:20 +0800

    added command

--- app/Console/Commands/DueMedicalCommand.php
+++ app/Console/Commands/DueMedicalCommand.php
@@ -8,6 +8,7 @@ use App\Models\Notification;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class DueMedicalCommand extends Command
@@ -31,41 +32,45 @@ class DueMedicalCommand extends Command
      */
     public function handle()
     {
-        $hr = User::where('role', 'hr')->first();
+        try {
+            $hr = User::where('role', 'hr')->first();
 
-        $leaveWithDueMedical = Leave::with('user:id,first_name,middle_name,last_name,email')
-            ->doesntHave('medical')
-            ->where('type', 'sick')
-            ->where(function ($query) {
-                $query->whereDate('from', '<=', Carbon::today()->subDays(3))
-                    ->whereNull('to')
-                    ->orWhereNotNull('to')
-                    ->whereDate('to', '<=', Carbon::today()->subDays(3));
-            })
-            ->whereNull('notifiedDueMedical')
-            ->get(['id', 'user_id']);
+            $leaveWithDueMedical = Leave::with('user:id,firstname,middlename,lastname,email')
+                ->doesntHave('medical')
+                ->where('type', 'sick')
+                ->where(function ($query) {
+                    $query->whereDate('from', '<=', Carbon::today()->subDays(3))
+                        ->whereNull('to')
+                        ->orWhereNotNull('to')
+                        ->whereDate('to', '<=', Carbon::today()->subDays(3));
+                })
+                ->whereNull('notifiedDueMedical')
+                ->get(['id', 'user_id']);
 
-        $leaveWithDueMedical->each(function ($leave) use ($hr) {
-            $leave->notifiedDueMedical = true;
-            $leave->saveQuietly();
+            $leaveWithDueMedical->each(function ($leave) use ($hr) {
+                $leave->notifiedDueMedical = true;
+                $leave->saveQuietly();
 
-            Notification::create([
-                'user_id' => $leave->user->id,
-                'type' => 'medical',
-                'details' => collect([
-                    'link' => route('leave.view', [$leave->id]),
-                    'name' => 'HR',
-                    'avatar' => $hr->avatar,
-                    'message' => ': Your medical certificate was due for submission.'
-                ])->toArray()
-            ]);
+                Notification::create([
+                    'user_id' => $leave->user->id,
+                    'type' => 'medical',
+                    'details' => collect([
+                        'link' => route('leave.view', [$leave->id]),
+                        'name' => 'HR',
+                        'avatar' => $hr->avatar,
+                        'message' => ': Your medical certificate was due for submission.'
+                    ])->toArray()
+                ]);
 
-            Mail::to($leave->user->email)
-                ->send(new EmailNotification(
-                    'Medical Certificate Due',
-                    'duemedical',
-                    ['name' => $leave->user->full_name]
-                ));
-        });
+                Mail::to($leave->user->email)
+                    ->send(new EmailNotification(
+                        'Medical Certificate Due',
+                        'duemedical',
+                        ['name' => $leave->user->full_name]
+                    ));
+            });
+        } catch (\Throwable $th) {
+            Log::info($th->getMessage());
+        }
     }
 }
